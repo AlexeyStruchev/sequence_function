53c2091918ce05c14522638638f39d5fba64f6ee
Aleksei Struchev <astruchev@gmail.com>
2022-11-26 23:05:57 +0300

    - corrected zero assert - return type must be list - fixed asserts - actual result now is set to be on first place

--- first.py
+++ first.py
@@ -6,7 +6,7 @@ def get_even_nums(input_number_: int) -> list:
     """
     if input_number_ == 0:
         print("Input number can't be zero")
-        return 0
+        return [0]
     elif input_number_ > 0:
         input_number_ = list(range(input_number_))
         return [number for number in input_number_ if number % 2 == 0]
@@ -16,12 +16,11 @@ def get_even_nums(input_number_: int) -> list:
 
 
 def check_actual_result(expected_result, actual_result):
-    assert expected_result == actual_result, f'Expected {expected_result}, got {actual_result}'
-    assert expected_result is not None, f'Expected {expected_result}, got {actual_result}'
-    assert actual_result != 0, f"Input can't be zero"
+    assert actual_result == expected_result, f'Expected {expected_result}, got {actual_result}'
+    assert actual_result is not None, f'Expected {expected_result}, got {actual_result}'
+    assert actual_result != [0], f"Input can't be zero"
 
 
 check_actual_result([0, 2, 4, 6, 8], get_even_nums(10))
 check_actual_result([-4, -2], get_even_nums(-4))
-check_actual_result(0, get_even_nums(0))
-
+check_actual_result([0], get_even_nums(0))
